9b2388d5323372d69e1cb07ce1d7ca62f4117439
Alejandro Cruz <alejandro.cruz@outlook.com>
2021-06-23 20:04:35 -0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+    application
+}
+
+group = "me.alex"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.lets-plot:lets-plot-kotlin-jvm:3.0.1")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,61 @@
+import jetbrains.datalore.plot.PlotHtmlExport
+import jetbrains.datalore.plot.PlotHtmlHelper
+import jetbrains.letsPlot.Stat
+import jetbrains.letsPlot.export.ggsave
+import jetbrains.letsPlot.geom.geomHistogram
+import jetbrains.letsPlot.ggsize
+import jetbrains.letsPlot.intern.Plot
+import jetbrains.letsPlot.intern.toSpec
+import jetbrains.letsPlot.letsPlot
+import java.awt.Desktop
+import java.io.File
+
+fun main(args: Array<String>) {
+    val inputFileName = "src/main/resources/hash.txt"
+    val input = readFileAsLinesUsingBufferedReader(inputFileName).map { it.toInt() }.toIntArray()
+    val result = hash(input)
+    println(result.contentToString())
+
+    //plot records
+    val p = plot(result)
+
+    // Export to HTML.
+    // Note: if all you need is to save HTML to a file than you can just use the 'ggsave()' function.
+    val content = PlotHtmlExport.buildHtmlFromRawSpecs(p.toSpec(), PlotHtmlHelper.scriptUrl("2.0.4"))
+    ggsave(p, "myPlot.svg")
+    openInBrowser(content)
+
+}
+
+fun hash(input: IntArray): IntArray {
+    return input.map { it.mod(1009) }.toIntArray()
+}
+
+fun plot(result: IntArray): Plot {
+    val data = mapOf(
+        "hash" to List(result.size) { "A" },
+        "index" to result
+    )
+
+    var p = letsPlot(data) { x = "index" }
+    p += geomHistogram(binWidth = 50) + ggsize(1500, 700)
+    return p
+}
+
+fun openInBrowser(content: String) {
+    val dir = File(System.getProperty("user.dir"), "lets-plot-images")
+    dir.mkdir()
+    val file = File(dir.canonicalPath, "my_plot.html")
+    file.createNewFile()
+    file.writeText(content)
+
+    Desktop.getDesktop().browse(file.toURI())
+}
+
+/**
+ * Read file
+ *
+ * @param fileName Read file
+ * @return List of strings read
+ */
+fun readFileAsLinesUsingBufferedReader(fileName: String): List<String> = File(fileName).bufferedReader().readLines()
